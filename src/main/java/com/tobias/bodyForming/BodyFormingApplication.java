eb40878f23ecb674cdadc6f374ac1ffa679fe64c
Saunaheiss <tobias.tomczak@gmail.com>
2017-07-03 11:26:55 +0200

    First commit

---
+++ src/main/java/com/tobias/bodyForming/BodyFormingApplication.java
@@ -0,0 +1,12 @@
+package com.tobias.bodyForming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BodyFormingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BodyFormingApplication.class, args);
+	}
+}
---
+++ src/test/java/com/tobias/bodyForming/BodyFormingApplicationTests.java
@@ -0,0 +1,16 @@
+package com.tobias.bodyForming;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BodyFormingApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
